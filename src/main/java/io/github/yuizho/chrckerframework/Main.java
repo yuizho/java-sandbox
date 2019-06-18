1980f64512bcf46da558431bbb842c02e919addb
yuizho <yuizho3@gmail.com>
2019-06-18 23:07:07 +0900

    add chcker framework sample code

---
+++ src/main/java/io/github/yuizho/chrckerframework/Main.java
@@ -0,0 +1,25 @@
+package io.github.yuizho.chrckerframework;
+
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+import java.util.Objects;
+
+/**
+ * https://checkerframework.org/
+ */
+public class Main {
+    public static void main(String... args) {
+        Integer result = getLength("aa");
+        // ここで警告が出る
+        System.out.println(result.intValue());
+    }
+
+    @Nullable
+    public static Integer getLength(@Nullable String str) {
+        // requreNonNullでも警告は消える
+        Objects.requireNonNull(str);
+        // 無駄にボクシングしているが, これはNullにするため
+        // strのNullCheckをしてい無いと警告がでる。
+        return str.length();
+    }
+}

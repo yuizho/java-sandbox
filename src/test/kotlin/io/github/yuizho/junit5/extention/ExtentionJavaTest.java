8e9a2851757e2d24996e17870ffaca82d88ba250
yuizho <yuizho3@gmail.com>
2020-02-08 15:35:28 +0900

    modify JUnit5 Extention

--- src/test/kotlin/io/github/yuizho/junit5/extention/ExtentionJavaTest.java
+++ src/test/kotlin/io/github/yuizho/junit5/extention/ExtentionJavaTest.java
@@ -1,16 +1,33 @@
 package io.github.yuizho.junit5.extention;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.mariadb.jdbc.MariaDbDataSource;
 
-@ExtendWith(SampleExtention.class)
+import java.sql.SQLException;
+
 public class ExtentionJavaTest {
+
+    @RegisterExtension
+    final SampleExtention sampleExtention;
+
+    {
+        MariaDbDataSource dataSource = new MariaDbDataSource();
+        try {
+            dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/test?characterEncoding=UTF-8");
+            dataSource.setUser("test");
+            dataSource.setPassword("password");
+        } catch (SQLException e) {
+        }
+        sampleExtention = new SampleExtention(dataSource);
+    }
+
     @Test
     @Preparation(testData = {
-            @Table(name = "hoge", rows = {
-                    @Row(values = {
-                            @Column(key = "id", value = "1"),
-                            @Column(key = "name", value = "hoge")
+            @Table(name = "division", rows = {
+                    @Row(vals = {
+                            @Col(name = "id", value = "4", isId = true),
+                            @Col(name = "name", value = "hoge")
                     })
             })
     })

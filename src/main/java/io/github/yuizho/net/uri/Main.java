e8cd8865159bcb2f00bdea1cb2742c8a1dfddd2a
yuizho <yuizho3@gmail.com>
2020-06-14 00:07:46 +0900

    modify uri sample

--- src/main/java/io/github/yuizho/net/uri/Main.java
+++ src/main/java/io/github/yuizho/net/uri/Main.java
@@ -4,7 +4,16 @@ import java.net.URI;
 
 public class Main {
     public static void main(String... args) {
-        URI uri1 = URI.create("http://example.com/").resolve("aaaa");
-        System.out.println(uri1);
+        var uri1 = URI.create("http://example.com/path/").resolve("aaaa");
+        System.out.println(uri1); // http://example.com/path/aaaa
+
+        var uri2 = URI.create("http://example.com/path").resolve("aaaa");
+        System.out.println(uri2); // unexpected join: http://example.com/aaaa
+
+        var uri3 = URI.create("http://example.com/path").resolve("/aaaa");
+        System.out.println(uri3); // unexpected join: http://example.com/aaaa
+
+        var uri4 = URI.create("http://example.com/path").resolve("/aaaa");
+        System.out.println(uri4); // unexpected join: http://example.com/aaaa
     }
 }

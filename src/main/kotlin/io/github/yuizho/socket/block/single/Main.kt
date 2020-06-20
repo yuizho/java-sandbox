b4b31d03e12d1855daa484ccf4bfc1609ee711c9
yuizho <yuizho3@gmail.com>
2020-06-21 00:18:15 +0900

    modify blocksingle thread socket sample

--- src/main/kotlin/io/github/yuizho/socket/block/single/Main.kt
+++ src/main/kotlin/io/github/yuizho/socket/block/single/Main.kt
@@ -5,21 +5,23 @@ import java.nio.channels.ServerSocketChannel
 
 fun main() {
     ServerSocketChannel.open().use { ssc ->
-        ssc.socket().bind(InetSocketAddress(8080))
-        println("start server")
+        val port = 8080
+        ssc.socket().bind(InetSocketAddress(port))
+        println("start server on port $port")
         while (true) {
             ssc.accept().use { sc ->
-                sc.socket().getInputStream().bufferedReader().use { reader ->
+                sc.socket().getInputStream().bufferedReader().also { reader ->
                     reader.forEachLine { line ->
                         println("received: $line")
                         if ("exit" == line) {
                             sc.socket().close()
+                            return@forEachLine
                         }
-                        sc.socket().getOutputStream().bufferedWriter().use { writer ->
-                            writer.write(line!!)
+                        sc.socket().getOutputStream().bufferedWriter().also { writer ->
+                            writer.write("$line\n")
+                            writer.flush()
                         }
                     }
-
                 }
             }
         }
